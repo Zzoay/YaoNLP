d733a250fd1d8b39c42eb010f7dbd33810614404
Yaozz <jianggongyao@gmail.com>
2020-11-24 16:01:54 +0800

    refactor: refactorize the examples dir tree

--- examples/text_classificaiton/run.py
+++ examples/text_classificaiton/run.py
@@ -13,13 +13,15 @@ from yaonlp.config_loader import Config
 from model import TextCNN
 
 
-torch.cuda.set_device(0)
+# torch.cuda.set_device(0)
 
 
 if __name__ == "__main__":
-    data_config = config_loader.load_config("text_classificaiton_example/config/data.json")
-    model_config = config_loader.load_config("text_classificaiton_example/config/model.json")
-    trainer_config = config_loader.load_config("text_classificaiton_example/config/trainer.json")
+    import torch
+    torch.__version__
+    data_config = config_loader.load_config("examples/text_classificaiton/config/data.json")
+    model_config = config_loader.load_config("examples/text_classificaiton/config/model.json")
+    trainer_config = config_loader.load_config("examples/text_classificaiton/config/trainer.json")
 
     train_dataset = MyDataset(data_config, train=True)
     test_dataset = MyDataset(data_config, train=False)
@@ -28,6 +30,6 @@ if __name__ == "__main__":
     test_iter = MyDataLoader(test_dataset, config=data_config)
     
     model = TextCNN(model_config)
-    trainer = Trainer(model, trainer_config)
+    trainer = Trainer(trainer_config)
 
-    trainer.train(train_iter)
+    trainer.train(model, train_iter)
