5a6bbb855cbe0e3a42dbee250f32faecc165dcfa
Yaozz <jianggongyao@gmail.com>
2020-12-09 19:50:31 +0800

    feat: add dependency parser eval func

--- examples/dependency_parsing/trainer.py
+++ examples/dependency_parsing/trainer.py
@@ -1,13 +1,14 @@
 
-from typing import Callable
+from typing import Callable, Optional
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from yaonlp.utils import to_cuda 
 from yaonlp.optim import OptimChooser
 from yaonlp.trainer import Trainer
+from yaonlp.data import MyDataLoader
+
 
 class MyTrainer(Trainer):
     def __init__(self, 
@@ -21,7 +22,9 @@ class MyTrainer(Trainer):
 
     def train(self, 
               model: nn.Module, 
-              train_iter: torch.utils.data.DataLoader) -> None:
+              train_iter: MyDataLoader, 
+              val_iter: Optional[MyDataLoader] = None) -> None:
+        model.train()
         if self.config["cuda"] and torch.cuda.is_available():
             model.cuda()
 
@@ -54,5 +57,37 @@ class MyTrainer(Trainer):
                 if step % self.config['print_every'] == 0:
                     print(f"--epoch {epoch}, step {step}, loss {loss}")
                     print(f"  {metrics}")
+
+                if val_iter and step % self.config['print_every'] == 0:
+                    self.eval(model, val_iter)
+
                 step += 1
         print("--training finished.")
+
+    # eval func
+    def eval(self, model: nn.Module, eval_iter: MyDataLoader) -> None:
+        model.eval()
+
+        avg_loss, avg_uas, avg_las = 0.0, 0.0, 0.0
+        for batch in eval_iter:
+            words, tags, heads, rels, masks, seq_lens = batch
+
+            if self.config["cuda"] and torch.cuda.is_available():
+                words, tags, heads, rels, masks, seq_lens = to_cuda(data=(words, tags, heads, rels, masks, seq_lens))
+
+            arc_logits, rel_logits = model(words, tags, heads, seq_lens)
+
+            loss = self.loss_fn(arc_logits, rel_logits, heads, rels, masks)
+            avg_loss += loss
+
+            metrics = self.metrics_fn(arc_logits, rel_logits, heads, rels, masks)
+
+            avg_uas += metrics['UAS']
+            avg_las += metrics['LAS']
+
+        size = eval_iter.data_size
+        avg_loss /= size
+        avg_uas /= size
+        avg_las /= size 
+        print("--Evaluation:")
+        print("-loss: {}  UAS: {}  LAS: {} \n".format(avg_loss, avg_uas, avg_las))
